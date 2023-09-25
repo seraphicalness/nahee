79c67f0771278f5e3900daedceb8c5ac97862c20
nahee <sally40929@naver.com>
2023-09-25 18:15:24 +0900

    V1.0

---
+++ week03/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	println("Hello Go!")
+	fmt.Println("OpenSourse")
+}
+
+// cd week03
+//go mod init week03 -> go.mod  모듈 파일 생김
+// 저장하기
+// go run .\main.go
