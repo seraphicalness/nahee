9a80674a42287a3bb8462c0bfbed478947800038
nahee <sally40929@naver.com>
2023-11-13 16:59:41 +0900

    v2.8

--- week12/main.go
+++ week12/main.go
@@ -3,8 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	var s []int
-	s = make([]int, 5) // 값이 없으니까 0으로만 나옴
+	// var s []int
+	// s = make([]int, 5) // 값이 없으니까 0으로만 나옴
+
+	// s := make([]int, 5) // 단축변수 사용, 변수 선언과 동시에 메모리 할당, 원소들은 제로 값으로 초기화
+
+	s := []int{0, 0, 0, 0, 0} // 단축 연산자 사용, 변수 선언과 동시에 메모리 할당, 원소 초기화
+
+	s[4] = 100
+	s[0] = 7
+	s[1] = 91
 
 	for _, value := range s {
 		fmt.Println(value)
