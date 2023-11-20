f47dab5fef58c4337fc141cfe2857d5474247f5c
nahee <sally40929@naver.com>
2023-11-20 16:50:36 +0900

    v3.4

--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import "fmt"
 
 func main() {
 	var a []string
+	var b []bool
 
-	fmt.Printf("%#v\n", a)
+	a = make([]string, 4, 5)
+	fmt.Printf("%#v %#v\n", a, b)
 	fmt.Println(a, len(a), cap(a))
+	fmt.Println(b, len(b), cap(b))
 
 }
